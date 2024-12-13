cf674cbdd4ca3c28a54a067c4598cbad8f48890d
Gourav <145884009+Gourav7568@users.noreply.github.com>
2024-12-13 11:02:06 +0530

    Update db.js

--- server/db.js
+++ server/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const { Schema } = mongoose;
 require("dotenv").config();
-const mongoUrl = process.env.DB_URL || 'mongodb+srv://gouravkumarsharmacse25:DPMJOJr7KYjkzYit@cluster0.aqbpr.mongodb.net/myDatabase?retryWrites=true&w=majority';
+const mongoUrl = 'mongodb://localhost:27017/moorti';
 mongoose.connect(mongoUrl, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
