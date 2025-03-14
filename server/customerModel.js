a213daf52fdddd261187db91f9930feb35ffcc3b
Gourav <145884009+Gourav7568@users.noreply.github.com>
2025-03-14 10:49:01 +0530

    Update customerModel.js

--- server/customerModel.js
+++ server/customerModel.js
@@ -1,1 +1,23 @@
+const mongoose = require("mongoose");
+
+const customerSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true, 
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  phone: {
+    type: Number,
+    required: true,
+  },
+});
+
+
+const Customer =
+  mongoose.models.Customer || mongoose.model("Customer", customerSchema);
+
+module.exports = Customer;
 
